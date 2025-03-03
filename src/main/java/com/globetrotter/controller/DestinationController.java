e848e781e4108cda529b5d9e57266ea8dea05476
Samridh Khanna <samridhkhanna@Samridhs-MacBook-Air.local>
2025-03-03 13:23:29 +0530

    Updated files with latest changes

--- src/main/java/com/globetrotter/controller/DestinationController.java
+++ src/main/java/com/globetrotter/controller/DestinationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/destinations")
 public class DestinationController {
--- src/main/java/com/globetrotter/controller/ShareController.java
+++ src/main/java/com/globetrotter/controller/ShareController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/share")
 public class ShareController {
