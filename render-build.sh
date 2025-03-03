6b80be551414a0b0c49f075f65b48b0132679d56
Samridh Khanna <samridhkhanna@Samridhs-MacBook-Air.local>
2025-03-03 09:04:42 +0530

    Fix JAVA_HOME for Render

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+export JAVA_HOME=/opt/render/project/.render/java
 echo "Using JAVA_HOME: $JAVA_HOME"
+
 chmod +x mvnw
 ./mvnw clean package
-
